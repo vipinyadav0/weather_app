c697a6d2d96b182ba6e6ace83855c723e3764815
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-12-07 14:03:20 +0530

    Weather API

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=20, null=True, blank=True)
+
+
+    class Meta:
+        verbose_name = ("City")
+        verbose_name_plural = ("Citys")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("City_detail", kwargs={"pk": self.pk})
+
--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
 from django.views import View
+import requests
+from .models import City
+from .forms import CityForm
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    url = "https://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=d71da0aca1d0c06fae26e1b1dc434022"
+    city = "Las vegas"
+    city_weather = requests.get(url.format(city)).json()
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+    form = CityForm()
+    
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather) 
+
+    
+
+
+    context = {
+        'weather_data' : weather_data,
+        'form': form
+}
+    return render(request, 'weather/index.html', context)
+    
